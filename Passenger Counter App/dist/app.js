ebc2bca0a2190d67d0e58937d506f5720a42d446
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-06 19:02:26 -0500

    10/6/2021
    Changed text color, added a save function, added interactive ability when save button is clicked, it logs it as an entry and resets the counter.

--- Passenger Counter App/dist/app.js
+++ Passenger Counter App/dist/app.js
@@ -1,7 +1,28 @@
-// document.getElementById("count-el").innerHTML = 5
+//initialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
 
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
-    console.log('the button was clicked')
+    count += 1
+    countEl.textContent = count
+    console.log(count)
 }
+
+function save() {
+    let entry = count + " - "
+    console.log(count)
+    saveEl.textContent += entry
+    countEl.textContent = 0
+    count = 0
+}
+
+// let name = "Tiara"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+
+// console.log(myGreeting)
